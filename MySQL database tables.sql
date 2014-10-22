129e928c3ce5a43b55556bbd50575e2519e133a8
PEMapModder <pemapmodder1970@gmail.com>
2014-10-22 19:01:27 +0800

    Added stats table

--- MySQL database tables.sql
+++ MySQL database tables.sql
@@ -1,3 +1,4 @@
+-- run these the first time
 CREATE TABLE players (
   uid INT PRIMARY KEY,
   names VARCHAR(1024),
@@ -16,3 +17,22 @@ CREATE TABLE kitpvp (
   deaths INT,
   kit SMALLINT
 );
+CREATE TABLE ids (
+  name VARCHAR(8) PRIMARY KEY,
+  id INT DEFAULT 0
+);
+INSERT INTO ids (name, id) VALUES ('uid', 0);
+CREATE TABLE stats (
+  title VARCHAR(64),
+  hour SMALLINT,
+  average DOUBLE DEFAULT 0,
+  total SMALLINT DEFAULT 0
+);
+-- for 0 to 23 for :d,
+INSERT INTO stats (title, hour) VALUES ('Number of joins of LegionPE', :d);
+INSERT INTO stats (title, hour) VALUES ('Number of joins of KitPvP', :d);
+INSERT INTO stats (title, hour) VALUES ('Number of joins of Parkour', :d);
+INSERT INTO stats (title, hour) VALUES ('Number of joins of Infected', :d);
+
+-- query script for graphing number of joins, title :s;
+SELECT hour,average FROM stats WHERE title = :s;
